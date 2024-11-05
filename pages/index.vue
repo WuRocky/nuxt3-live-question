dbfd10c51d6440b0cc9906134e2c4e3202bf67ca
Rocky Wu <rocky_wu@everrich.com.tw>
2024-11-05 14:18:23 +0800

    day2-homework

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,27 @@
 
 <template>
   <div>
-    Page: 首頁
+    <div>
+      <h1>Page: index</h1>
+    </div>
+    <button type="button" class="btn btn-primary">Primary</button>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+    <button type="button" class="btn btn-success">Success</button>
+    <button type="button" class="btn btn-danger">Danger</button>
+    <button type="button" class="btn btn-warning">Warning</button>
+    <button type="button" class="btn btn-info">Info</button>
+    <button type="button" class="btn btn-light">Light</button>
+    <button type="button" class="btn btn-dark">Dark</button>
+    
+    <button type="button" class="btn btn-link">Link</button>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped  lang="scss">
+$primary: #0F6DF2;
+h1 {
+  color: $primary;
+  margin-bottom: 20px;
+}
+
+</style>
